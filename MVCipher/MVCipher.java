e9cc1428500bc530be15aafc43e5f2a196ac041a
danicah17 <danica.rummy@gmail.com>
2024-09-26 23:22:46 -0700

    Add files via upload

--- MVCipher/MVCipher.java
+++ MVCipher/MVCipher.java
@@ -1,11 +1,13 @@
 // imports go here
+import java.util.Scanner;
+import java.io.PrintWriter;
 
 /**
- *	MVCipher - Add your description here
+ *	MVCipher - Encrypts or decrypts a file based on the user's choice
  *	Requires Prompt and FileUtils classes.
  *	
  *	@author Danica Huang	
- *	@since	September 
+ *	@since	September 20th, 2024
  */
 public class MVCipher {
 	
@@ -25,35 +27,43 @@ public class MVCipher {
 	public void run() {
 		System.out.println("\n Welcome to the MV Cipher machine!\n");
 		boolean done = false;
+		String word = "";
 		
 		while (!done) {
-			word = Prompt.getString("Please input a word to use as key (letters only)");
-			wordLength = word.length();
-			if (word.length > 2) {
-				word = word.toUpperCase();
-				int input = Prompt.getInt("Encrypt or decrypt?", 1, 2);
-				if (input == 1) {
-					encrypt();
-					done = true;
-					encryptFileName = Prompt.getString("Name of file to encrypt");
-				}
-				else (input == 2) {
-					decrypt();
-					done = true;
-				}
-			else {
-				System.out.println("ERROR: Key must be all letters and at least 3 characters long");
+			word = Prompt.getString("Please input a word to use as key "+
+			"(letters only)");
+			word = word.toUpperCase();
+			done = true;
+			if (word.length() < 3) {
+				done = false;
 			}
-				
-		
-		
-		/* Prompt for a key and change to uppercase
+			for (int i = 0; i < word.length(); i++) {
+				char current = word.charAt(i);
+				if (!('A' <= current && current <='Z')) {
+					done = false;
+				}
+			}
+			if (!done) {
+				System.out.println("ERROR: Key must be all letters"+
+				" and at least 3 characters long");
+			}
+		}
+	
+			/* Prompt for a key and change to uppercase
 		   Do not let the key contain anything but alpha
 		   Use the Prompt class to get user input */
 		
 		
 		/* Prompt for encrypt or decrypt */
-			
+			int input = Prompt.getInt("Encrypt or decrypt?", 1, 2);
+			String inputFileName = "";
+			if (input == 1) {
+				inputFileName = Prompt.getString("Name of file to encrypt");
+			}
+			else if (input == 2) {
+				inputFileName = Prompt.getString("Name of file to decrypt");
+			}
+			String outputFileName = Prompt.getString("Name of output file");
 			
 		/* Prompt for an input file name */
 		
@@ -62,19 +72,80 @@ public class MVCipher {
 		
 		
 		/* Read input file, encrypt or decrypt, and print to output file */
-		
+		Scanner scanner = FileUtils.openToRead(inputFileName);
+		PrintWriter writer = FileUtils.openToWrite(outputFileName);
+		if (input == 1) {
+			encrypt(scanner, writer, word);
+		}
+		else if (input == 2) {
+			decrypt(scanner, writer, word);
+		}
 		
 		/* Don't forget to close your output file */
 	}
 	
 	// other methods go here
-	public void encrypt() {
-	...
+	
+	/**
+	 *	Method header goes here
+	 */
+	public void encrypt(Scanner scanner, PrintWriter writer, String word) {
+		int keyCounter = 0;
+		while (scanner.hasNext()) {
+			String line = scanner.nextLine();
+			for (int c = 0; c < line.length(); c++) {
+				char i = line.charAt(c);
+				char k = word.charAt(keyCounter);
+				int shift = k - 'A' + 1;
+				char offset = '?';
+				if ('a' <= i && i <='z') {
+					offset = 'a';
+				}
+				else if ('A' <= i && i <='Z') {
+					offset = 'A';
+				}
+				else {
+					writer.print(i);
+				}
+				if (offset != '?') {
+					int n = i - offset;
+					int x = (n + shift) % 26;
+					char e = (char)(x + offset);
+					writer.print(e);
+					keyCounter = (keyCounter + 1) % word.length();
+				}
+			}
+			writer.println();
+		}
 	}
 	
-	public void decrypt() {
-		...
+	public void decrypt(Scanner scanner, PrintWriter writer, String word) {
+		int keyCounter = 0;
+		while (scanner.hasNext()) {
+			String line = scanner.nextLine();
+			for (int c = 0; c < line.length(); c++) {
+				char inputChar = line.charAt(c);
+				char keyChar = word.charAt(keyCounter);
+				int shift = k - 'A' + 1;
+				char offset = '?';
+				if ('a' <= i && i <='z') {
+					offset = 'a';
+				}
+				else if ('A' <= i && i <='Z') {
+					offset = 'A';
+				}
+				else {
+					writer.print(i);
+				}
+				if (offset != '?') {
+					int origIndex = i - offset;
+					int finalIndez = (n - shift + 26) % 26;
+					char finalChar = (char)(x + offset);
+					writer.print(e);
+					keyCounter = (keyCounter + 1) % word.length();
+				}
+			}
+			writer.println();
+		}
 	}
-
-	
 }
